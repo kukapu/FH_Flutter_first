250f747e8eb6ca267de18d96e1c4460ab97ad8c5
Albert Alvarez Estelles <114157589+kukapu@users.noreply.github.com>
2023-06-29 13:49:11 +0200

    first commit

---
+++ 01-dart/01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  var myName = 'Fernando';
+  late final myName;
+  const myName = 'Fernando';
+
+  String myName = 'Fernando';
+  
+  print('Hola ');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ' ${1 + 1});
+}
